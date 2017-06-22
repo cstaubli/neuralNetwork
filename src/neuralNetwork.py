9576e70a58e94cf43806ee171b917b3b8868ec39
Christian Staubli <christian.staubli@mickdunde.net>
2017-06-22 21:30:59 +0200

    initial commit

---
+++ src/neuralNetwork.py
@@ -0,0 +1,26 @@
+import numpy
+
+class neuralNetwork:
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+        self.lr = learningrate
+
+        self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+
+        pass
+
+    def train():
+        pass
+
+    def query():
+        pass
+
+input_nodes = 3
+hidden_nodes = 3
+output_nodes = 3
+learning_rate = 0.3
+
+n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
