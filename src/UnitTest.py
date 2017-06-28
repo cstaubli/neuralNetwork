a9183d186d8cb6bed02f33008dd4c97456daeb5b
Christian Staubli <christian.staubli@mickdunde.net>
2017-06-28 21:14:19 +0200

    added proper unit test

--- src/UnitTest.py
+++ src/UnitTest.py
@@ -1,9 +1,26 @@
+import unittest
+import numpy
 from NeuralNetwork import NeuralNetwork
 
-input_nodes = 3
-hidden_nodes = 3
-output_nodes = 3
-learning_rate = 0.3
+class TestNeuralNetwork(unittest.TestCase):
+    "Unit test for NeuralNetwork"
+    def setUp(self):
+        self.inputs_list = [1.0, 0.5, -1.0]
+        self.input_nodes = 3
+        self.hidden_nodes = 3
+        self.output_nodes = 3
+        self.learning_rate = 0.3
 
-MyNN = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
-print(MyNN.query([1.0, 0.5, -1.5]))
+    def test_query(self):
+        "Test query method"
+        mynn = NeuralNetwork(
+            self.input_nodes,
+            self.hidden_nodes,
+            self.output_nodes,
+            self.learning_rate)
+        answer = mynn.query(self.inputs_list)
+        check = isinstance(answer, numpy.ndarray)
+        self.assertTrue(check)
+
+if __name__ == '__main__':
+    unittest.main()
