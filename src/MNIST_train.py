1fcec4a57af5b487f7da3c2ca113c34787ec2c8e
Christian Staubli <christian.staubli@gmail.com>
2017-07-10 21:55:52 +0200

    add loope for different learning rates

--- src/MNIST_train.py
+++ src/MNIST_train.py
@@ -2,11 +2,11 @@ import numpy as np
 # import matplotlib.pyplot
 from NeuralNetwork import NeuralNetwork
 
-data_file_train = open("../../Downloads/mnist_train.csv", "r")
+data_file_train = open("data/mnist_train_100.csv", "r")
 data_list_train = data_file_train.readlines()
 data_file_train.close()
 
-data_file_test = open("../../Downloads/mnist_test.csv", "r")
+data_file_test = open("data/mnist_test_10.csv", "r")
 data_list_test = data_file_test.readlines()
 data_file_test.close()
 
@@ -22,43 +22,44 @@ data_file_test.close()
 
 # number of input, hidden and output nodes
 INPUT_NODES = 784
-HIDDEN_NODES = 400
+HIDDEN_NODES = 200
 OUTPUT_NODES = 10
 
-# learning rate
-LEARNING_RATE = 0.2
+for lr in range(1, 9, 1):
+    # learning rate
+    LEARNING_RATE = lr * 0.1
 
-# create instance of neural network
-nn = NeuralNetwork(INPUT_NODES, HIDDEN_NODES, OUTPUT_NODES, LEARNING_RATE)
+    # create instance of neural network
+    nn = NeuralNetwork(INPUT_NODES, HIDDEN_NODES, OUTPUT_NODES, LEARNING_RATE)
 
-# epochs is the number of times the training data set is used for training
-EPOCHS = 5
+    # epochs is the number of times the training data set is used for training
+    EPOCHS = 5
 
-for e in range(EPOCHS):
-    for record in data_list_train:
-        all_values = record.split(',')
-        inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-        targets = np.zeros(OUTPUT_NODES) + 0.01
-        targets[int(all_values[0])] = 0.99
-        nn.train(inputs, targets)
+    for e in range(EPOCHS):
+        for record in data_list_train:
+            all_values = record.split(',')
+            inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+            targets = np.zeros(OUTPUT_NODES) + 0.01
+            targets[int(all_values[0])] = 0.99
+            nn.train(inputs, targets)
+            pass
         pass
-    pass
 
-SCORECARD = []
+    SCORECARD = []
 
-for record in data_list_test:
-    all_values = record.split(',')
-    correct_label = int(all_values[0])
-    inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-    outputs = nn.query(inputs)
-    label = np.argmax(outputs)
-    if (label == correct_label):
-        print("correct, found {0}".format(label))
-        SCORECARD.append(1)
-    else:
-        print("wrong, found {0} excpected {1}".format(label, correct_label))
-        SCORECARD.append(0)
+    for record in data_list_test:
+        all_values = record.split(',')
+        correct_label = int(all_values[0])
+        inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+        outputs = nn.query(inputs)
+        label = np.argmax(outputs)
+        if (label == correct_label):
+            # print("correct, found {0}".format(label))
+            SCORECARD.append(1)
+        else:
+            # print("wrong, found {0} excpected {1}".format(label, correct_label))
+            SCORECARD.append(0)
 
-scorecard_array = np.asarray(SCORECARD)
-perf = scorecard_array.sum() / (scorecard_array.size * 1.0)
-print ("performance = {0:5f}".format(perf))
+    scorecard_array = np.asarray(SCORECARD)
+    perf = scorecard_array.sum() / (scorecard_array.size * 1.0)
+    print ("performance = {0:5f}, learning rate {1:1f}".format(perf, LEARNING_RATE))
