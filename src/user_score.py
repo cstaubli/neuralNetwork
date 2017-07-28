d439d74bf88ac2f9af3b041441f28e7999bcc752
Christian Staubli <christian.staubli@gmail.com>
2017-07-28 14:52:06 +0200

    log duration

--- src/user_score.py
+++ src/user_score.py
@@ -1,6 +1,7 @@
 import numpy as np
 from NeuralNetwork import NeuralNetwork
 import scipy.misc
+import time
 
 data_file_train = open(name="../../Downloads/mnist_train.csv", mode="r")
 data_list_train = data_file_train.readlines()
@@ -19,6 +20,7 @@ OUTPUT_NODES = 10
 for lr in range(2, 9, 1):
     # learning rate
     LEARNING_RATE = lr * 0.05
+    print("setting learning rate to {0}".format(LEARNING_RATE))
 
     # create instance of neural networkr
     nn = NeuralNetwork(INPUT_NODES, HIDDEN_NODES, OUTPUT_NODES, LEARNING_RATE)
@@ -27,6 +29,7 @@ for lr in range(2, 9, 1):
     EPOCHS = 5
 
     for e in range(EPOCHS):
+        start_time = time.time()
         for record in data_list_train:
             all_values = record.split(',')
             # prevent zeroes as input
@@ -36,8 +39,8 @@ for lr in range(2, 9, 1):
             targets[int(all_values[0])] = 0.99
             # and train
             nn.train(inputs, targets)
-            pass
-        pass
+        duration = time.time() - start_time
+        print("run {0} is complete, duration was {1:0.2f} seconds".format(e, duration))
 
     # query with user picture
     outputs = nn.query(img_data)
